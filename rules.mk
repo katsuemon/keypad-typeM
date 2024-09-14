993755e666027010345cf7f40c7ea5fffc43ea55
katsuemon <katsuemon@gmail.com>
2024-09-14 11:28:18 +0900

    change rules

--- rules.mk
+++ rules.mk
@@ -3,10 +3,12 @@
 # Link Time Optimization required for size.
 LTO_ENABLE = yes
 
+OLED_ENABLE = no
+
 RGBLIGHT_ENABLE = yes
 RGBLIGHT_DRIVER = ws2812
 RGBLIGHT_SLEEP = yes
 
 VIA_ENABLE = yes
 WAIT_FOR_USB = yes
-
+TAP_DANCE_ENABLE = yes
